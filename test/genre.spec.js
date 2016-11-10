38e3727aa20cd6992d699361013e3d0ebdaa6e4b
Mark Robson <mark.o.robson@gmail.com>
2016-11-10 11:02:12 +0000

    Added more genre tests

--- test/genre.spec.js
+++ test/genre.spec.js
@@ -23,10 +23,11 @@ test('Genre method: should expand object', t => {
       },
       dog: {
         puppy: 'puppy',
-      }
-    }
+      },
+    },
   };
-  const petString = 'cat => cat,pet\nkitten => kitten,cat,pet\ndog => dog,pet\npuppy => puppy,dog,pet';
+  const petString =
+    'cat => cat,pet\nkitten => kitten,cat,pet\ndog => dog,pet\npuppy => puppy,dog,pet';
 
   t.equals(lib.genre(pets), petString);
   t.end();
@@ -47,11 +48,12 @@ test('Genre method: should expand deep nested object', t => {
         rat: 'rat',
         rabbit: 'rabbit',
         mouse: 'mouse',
-      }
-    }
+      },
+    },
   };
 
-  const petString = 'cat => cat,pet\nkitten => kitten,cat,pet\ndog => dog,pet\npuppy => puppy,dog,pet\nrodent => rodent,pet\nrat => rat,rodent,pet\nrabbit => rabbit,rodent,pet\nmouse => mouse,rodent,pet';
+  const petString =
+    'cat => cat,pet\nkitten => kitten,cat,pet\ndog => dog,pet\npuppy => puppy,dog,pet\nrodent => rodent,pet\nrat => rat,rodent,pet\nrabbit => rabbit,rodent,pet\nmouse => mouse,rodent,pet';
 
   t.equals(lib.genre(pets), petString);
   t.end();
